32997b960a80a7a895fe044237bd6f119757e2d2
Lisa R <reiber@mpib-berlin.mpg.de>
2019-12-01 10:17:34 +0100

    add standardize amount of trash by populations size

--- datenguideR_analysis.R
+++ datenguideR_analysis.R
@@ -10,7 +10,29 @@ library(datenguideR)
 ggstatsplot::set_cwd()
 
 # data
-(df <- dg_call(
+(df_population <- dg_call(
+  nuts_nr = 1,
+  stat_name = "BEV028",
+  year = c(
+    2006,
+    2007,
+    2008,
+    2009,
+    2010,
+    2011,
+    2012,
+    2013,
+    2014,
+    2015,
+    2016,
+    2017,
+    2018
+  )) %>%  
+  select(id, year, population = value)
+)
+
+
+(df_trash <- dg_call(
   nuts_nr = 1,
   stat_name = "AEW010",
   year = c(
@@ -27,17 +49,24 @@ ggstatsplot::set_cwd()
     2016,
     2017,
     2018
-  )
-))
+  )) %>% 
+    select(
+           trash = value,
+           everything(),
+           -name)
+)
+
 
 # writing the data file
 # (just because the API is not stable and sometimes returns NAs)
 # readr::write_csv(df, "datenguideR.csv")
 
 # extracting id names for each region
-df_trash <-
+df_combined <- df_trash %>% 
+  dplyr::left_join(x = .,
+                   y = df_population, by = c("id", "year")) %>% 
   dplyr::left_join(
-    x = dplyr::select(df, -name),
+    x = .,
     y = datenguideR::dg_regions %>%
       dplyr::filter(level == "nuts1") %>%
       dplyr::select(id, name),
@@ -45,56 +74,58 @@ df_trash <-
   )
 
 # creating means by name and year
-df_trash %<>%
+df_combined %<>%
   dplyr::group_by(name, year) %>%
-  dplyr::summarise(mean_trash = mean(value, na.rm = TRUE)) %>%
-  dplyr::ungroup()
+  dplyr::summarise(mean_trash = mean(trash, na.rm = TRUE), population = population) %>%
+  dplyr::ungroup() %>% 
+  mutate(mean_trash_std_ton = mean_trash / population,
+         mean_trash_std_kg = mean_trash_std_ton * 1000)
+
 
 # a broad take on the data
-ggsave(
-  filename = "datenGuideR.png",
-  plot = ggstatsplot::ggbetweenstats(
-    data = df,
-    x = year,
-    y = value,
-    title = "Trash produced by different regions in Germany",
-    xlab = "year",
-    ylab = "trash weight (tons)",
-    outlier.tagging = TRUE,
-    outlier.label = name,
-    outlier.coef = 2.5,
-    messages = FALSE,
-    bf.message = FALSE,
-    ggtheme = hrbrthemes::theme_ipsum_tw(),
-    package = "ggsci",
-    palette = "default_igv"
-  ),
-  width = 20,
-  height = 15
-)
+# ggsave(
+#   filename = "datenGuideR.png",
+#   plot = ggstatsplot::ggbetweenstats(
+#     data = df,
+#     x = year,
+#     y = value,
+#     title = "Trash produced by different regions in Germany",
+#     xlab = "year",
+#     ylab = "trash weight (tons)",
+#     outlier.tagging = TRUE,
+#     outlier.label = name,
+#     outlier.coef = 2.5,
+#     messages = FALSE,
+#     bf.message = FALSE,
+#     ggtheme = hrbrthemes::theme_ipsum_tw(),
+#     package = "ggsci",
+#     palette = "default_igv"
+#   ),
+#   width = 20,
+#   height = 15
+# )
 
 # adding cumulative sum
-df_trash %<>%
+df_combined %<>%
   dplyr::group_by(name) %>%
-  dplyr::mutate(cumsum = cumsum(mean_trash)) %>%
+  dplyr::mutate(cumsum = cumsum(mean_trash_std_kg)) %>%
   dplyr::ungroup()
 
 # creating a fancy visualization
 gganimate::anim_save(
-  filename = paste("trash.gif"),
-  animation = ggplot(
-    dplyr::mutate(df_trash, cumsum_mil = cumsum / 100000),
-    aes(name, cumsum_mil)
-  ) +
+  filename = "trash.gif",
+  animation = ggplot(df_combined,
+    aes(name, cumsum)
+    ) +
     geom_point(aes(color = name), size = 6) +
-    scale_y_continuous(breaks = seq(0, 3500, 500), limits = c(0, 3500)) +
     coord_flip() +
     ggthemes::theme_tufte() +
     theme(legend.position = "none", text = element_text(size = 20)) +
     guides(legend = FALSE) +
     labs(
-      title = "How much trash does Germany accumulate over time? \nYear: {frame_time}",
-      y = "cumulative sum of trash weight (in million tons)",
+      title = "How much trash does Germany accumulate over time? 
+      \n (Since 2006 to {frame_time}",
+      y = "cumulative sum of trash weight (in kilogram per capita)",
       x = "region"
     ) +
     transition_time(year) +
